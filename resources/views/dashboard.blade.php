6429c4756cae2dee017aa7653da36d194a22bef8
argaputra12 <argaputra@student.uns.ac.id>
2023-05-27 12:51:37 +0700

    fix navbar and  dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,68 @@
 <x-app-layout>
-    <x-slot name="header">
+  {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto px-24">
+      <div class="mx-auto bg-white flex flex-col justify-center gap-16">
+        <!-- Laboratorium SI -->
+        <div class="p-8 bg-gray-300 overflow-auto">
+          <p class="text-justify text-black text-lg">
+            Laboratorium Sistem Informasi merupakan salah satu laboratorium riset yang berada di bawah kelompok keilmuan
+            Applied Information System. . Laboratorium ini memfokuskan diri pada bidang pengembangan Sistem Informasi di
+            Fakultas Ilmu Terapan pada Program Studi Sistem Informasi. Laboratorium ini dibangun pada tahun 2017 dan
+            banyak digunakan untuk kegiatan riset dan study group. Pada tahun 2022 laboratorium sistem informasi
+            mendapatkan renovasi untuk menunjang kegiatannya. Dengan adanya renovasi ini diharapkan dapat meningkatkan
+            utilitas laboratorium. Adapun kegiatan yang dapat dilakukan di dalam laboratorium sistem informasi meliputi
+            penelitian, study group dan rapat.
+          </p>
         </div>
+
+        <!-- Pembina Lab -->
+        <div class="p-8 bg-gray-300 overflow-auto">
+          <div class="w-full flex flex-col justify-center items-center">
+            <div class="bg-black text-center text-white text-xl py-4 px-14 mb-10">
+              <h2>Pembina Laboratorium</h2>
+              <h2>di lingkungan KK AIS periode 2023</h2>
+            </div>
+
+          </div>
+          <div class="flex flex-col gap-3 text-lg px-6">
+            <div class="flex flex-col">
+              <p>1. Management of Information Systems</p>
+              <p class="pl-4">Suryatiningsih, S.T., M.T., OCA., C.Ht.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>2. Data Analytics Laboratory</p>
+              <p class="pl-4">Tora Fahrudin, S.T., M.T.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>3. Big Data Laboratory</p>
+              <p class="pl-4">Guntur Prabawa Kusuma, S.T., M.T., Ph.D.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>4. Enterprise Systems Laboratory</p>
+              <p class="pl-4">Rochmawati, S.T., M.T.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>5. IT Infrastructure and Security Laboratory</p>
+              <p class="pl-4">Pramuko Aji, S.T., M.T.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>6. Information System Research Laboratory</p>
+              <p class="pl-4">Patrick Adolf Telnoni, S.T., M.T.</p>
+            </div>
+            <div class="flex flex-col">
+              <p>7. Accounting Information System Research Laboratory</p>
+              <p class="pl-4">Anak Agung Gde Agung, S.T., M.T.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+  </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,10 @@
 
         <!-- Flowbite -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-        
+
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
