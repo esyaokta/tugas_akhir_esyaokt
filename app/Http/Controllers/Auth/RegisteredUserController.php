bb35b442f7ebf362b9de64d7368a2f9f7f2f3d4f
argaputra12 <argaputra@student.uns.ac.id>
2023-08-14 00:40:04 +0700

    change nim validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'nama' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255', 'unique:' . User::class],
-            'nim' => ['required', 'max:9999999999', 'unique:' . User::class, 'numeric'],
+            'nim' => ['required', 'max:9999999999', 'unique:' . User::class, 'numeric', 'starts_with:67'],
             'no_hp' => ['required', 'numeric'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
